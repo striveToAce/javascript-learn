f96d4c33880c9b6b1b3fe8308d2a42f9b6350adb
Ashish Kumar <ashish380431@gmail.com>
2024-11-02 15:10:33 +0530

    functional programming concept initiation and added

---
+++ functional-programming/index.js
@@ -0,0 +1,25 @@
+/*
+Core Concepts of Functional Programming
+>>Pure Functions
+>>Immutability
+>>First-Class Functions
+>>Higher-Order Functions
+>>Function Composition
+*/
+
+
+/*
+Pure Function: A function that always produces the same output given the same input and has no side effects.
+*/
+
+/*
+Immutability: Data cannot be modified after it is created. Instead, new data is created.
+*/
+
+/*
+First-Class Functions: Functions can be assigned to variables, passed as arguments, and returned from other functions.
+*/
+
+/*
+Higher-Order Function: A function that takes another function as an argument or returns a function.
+*/
